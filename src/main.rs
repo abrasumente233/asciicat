41f63a086ac3111242358f68245cfc5b7ef4c7d4
abrasumente <345396594@qq.com>
2024-02-02 23:22:14 +0800

    serialize response into a struct

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
+#[derive(serde::Deserialize, Debug)]
+struct CatImage {
+    id: String,
+    url: String,
+    width: u32,
+    height: u32,
+}
+
 #[tokio::main]
 async fn main() {
     let resp = reqwest::get("https://api.thecatapi.com/v1/images/search")
         .await
         .unwrap();
-    println!("Status: {}", resp.status());
-    let body = resp.text().await.unwrap();
-    println!("Body: {}", body);
+    if !resp.status().is_success() {
+        panic!("The request was not successful: {}", resp.status());
+    }
+
+    let cat_images: Vec<CatImage> = resp.json().await.unwrap();
+    let cat_image = cat_images
+        .first()
+        .expect("the cat image API should at least return one image");
+    println!("The cat image URL is: {}", cat_image.url);
 }
