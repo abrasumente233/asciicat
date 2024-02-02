13ff745558d5077cde40f2d588cf3c248be67776
abrasumente <345396594@qq.com>
2024-02-02 23:48:12 +0800

    chain url request

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use color_eyre::eyre::eyre;
+use pretty_hex::PrettyHex;
 use serde::Deserialize;
 
 #[tokio::main]
@@ -8,20 +9,18 @@ async fn main() {
 }
 
 async fn get_cat_url() -> color_eyre::Result<String> {
-    let resp = reqwest::get("https://api.thecatapi.com/v1/images/search").await?;
-    if !resp.status().is_success() {
-        return Err(eyre!("The request was not successful: {}", resp.status()));
-    }
-
     #[derive(Deserialize, Debug)]
     struct CatImage {
         url: String,
     }
 
-    let cat_images: Vec<CatImage> = resp.json().await?;
-    let Some(image) = cat_images.into_iter().next() else {
-        return Err(eyre!("The cat API did not return any images."));
-    };
-
-    Ok(image.url)
+    reqwest::get("https://api.thecatapi.com/v1/images/search")
+        .await?
+        .error_for_status()?
+        .json::<Vec<CatImage>>()
+        .await?
+        .into_iter()
+        .next()
+        .ok_or(eyre!("The cat API did not return any images."))
+        .map(|cat_image| cat_image.url)
 }
