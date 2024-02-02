4d474bfaf3d43d131fbcdb25b62f1fd20f5d4ec8
abrasumente <345396594@qq.com>
2024-02-02 23:55:36 +0800

    download images as bytes and dump via `PrettyHex`

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,19 @@ use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
-    let image_url = get_cat_url().await.unwrap();
-    println!("The cat image URL is: {}", image_url);
+    let image = get_cat_bytes().await.unwrap();
+    dbg!(image.len());
+    println!("{:?}", &image[..200].hex_dump());
+    //println!("The cat image URL is: {}", image_url);
 }
 
-async fn get_cat_url() -> color_eyre::Result<String> {
+async fn get_cat_bytes() -> color_eyre::Result<Vec<u8>> {
     #[derive(Deserialize, Debug)]
     struct CatImage {
         url: String,
     }
 
-    reqwest::get("https://api.thecatapi.com/v1/images/search")
+    let url = reqwest::get("https://api.thecatapi.com/v1/images/search")
         .await?
         .error_for_status()?
         .json::<Vec<CatImage>>()
@@ -22,5 +24,12 @@ async fn get_cat_url() -> color_eyre::Result<String> {
         .into_iter()
         .next()
         .ok_or(eyre!("The cat API did not return any images."))
-        .map(|cat_image| cat_image.url)
+        .map(|cat_image| cat_image.url)?;
+
+    Ok(reqwest::get(&url)
+        .await?
+        .error_for_status()?
+        .bytes()
+        .await?
+        .to_vec())
 }
