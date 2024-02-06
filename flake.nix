6ec01107a0f9c909af69af3295d3d69ed93e363f
abrasumente <345396594@qq.com>
2024-02-06 15:00:29 +0800

    nix: build rust and a docker image for it

--- flake.nix
+++ flake.nix
@@ -9,26 +9,64 @@
         flake-utils.follows = "flake-utils";
       };
     };
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
   };
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, crane }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-        isDarwin = pkgs.lib.strings.hasSuffix "-darwin" system;
         rustToolchain = pkgs.rust-bin.stable.latest.default;
-        nativeBuildInputs = with pkgs; [
-          rustToolchain
-          pkg-config
-        ] ++ lib.optional (isDarwin) darwin.apple_sdk.frameworks.SystemConfiguration;
-        buildInputs = with pkgs; [ openssl sqlite ];
+        isDarwin = pkgs.lib.strings.hasSuffix "-darwin" system;
+        craneLibWithoutRegistry = crane.lib.${system}.overrideToolchain rustToolchain;
+        shipyardToken = builtins.readFile ./secrets/shipyard;
+        craneLib = craneLibWithoutRegistry.appendCrateRegistries [
+          (craneLibWithoutRegistry.registryFromDownloadUrl {
+            indexUrl = "ssh://git@ssh.shipyard.rs/abrasumente/crate-index.git";
+            dl = "https://crates.shipyard.rs/api/v1/crates";
+            fetchurlExtraArgs = {
+              curlOptsList = [ "--header" "User-Agent: shipyard ${shipyardToken}" ];
+            };
+          })
+        ];
+        commandArgs = {
+          src = craneLib.cleanCargoSource (craneLib.path ./.);
+          nativeBuildInputs = with pkgs; [
+            rustToolchain
+            pkg-config
+          ] ++ lib.optional (isDarwin) darwin.apple_sdk.frameworks.SystemConfiguration;
+          buildInputs = with pkgs; [ openssl sqlite ];
+        };
+        cargoArtifacts = craneLib.buildDepsOnly commandArgs;
+        bin = craneLib.buildPackage commandArgs // {
+          # TODO: commandArgs is stupid...
+          inherit cargoArtifacts;
+        };
+        dockerImage = pkgs.dockerTools.buildImage {
+          name = "asciicat";
+          tag = "latest";
+          copyToRoot = [ bin ];
+          config = {
+            # TODO: why prefix with ${bin}?
+            Cmd = [ "${bin}/bin/asciicat" ];
+          };
+        };
       in
       with pkgs;
       {
+        packages = {
+          inherit bin dockerImage;
+          default = bin;
+        };
         devShells.default = mkShell {
-          inherit buildInputs nativeBuildInputs;
+          inputsFrom = [ bin ];
         };
       });
 }
