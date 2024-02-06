9bfe98db066e6693ff242ee65666f8ac6ba4c421
abrasumente <345396594@qq.com>
2024-02-06 16:49:45 +0800

    see if our magic GA nix cache works

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,7 @@ async fn main() {
     ));
 
     let (_honeyguard, _tracer) = opentelemetry_honeycomb::new_pipeline(
-        std::env::var("HONEYCOMB_API_KEY").expect("$HONEYCOMB_API_KEY should be set"),
+        std::env::var("HONEYCOMB_API_KEY").expect("$HONEYCOMB_API_KEY must be set"),
         "asciicat".into(),
     )
     .install()
@@ -55,8 +55,8 @@ async fn main() {
     };
 
     let country_db_path =
-        std::env::var("GEOLITE2_COUNTRY_DB").expect("$GEOLITE2_COUNTRY_DB should be set");
-    let analytics_db_path = std::env::var("ANALYTICS_DB").expect("$ANALYTICS_DB should be set");
+        std::env::var("GEOLITE2_COUNTRY_DB").expect("$GEOLITE2_COUNTRY_DB must be set");
+    let analytics_db_path = std::env::var("ANALYTICS_DB").expect("$ANALYTICS_DB must be set");
     let state = ServerState {
         client: reqwest::Client::default(),
         locat: Arc::new(Locat::new(&country_db_path, &analytics_db_path).unwrap()),
